f96acc19f2b6f7f94c882b3780246b11909d9da4
PatternsInNature <jyrki.jylha@sitowise.com>
2019-02-06 22:58:52 +0200

    Debugging

--- dist/lib/convert.js
+++ dist/lib/convert.js
@@ -3,7 +3,7 @@ const rp = require("request-promise");
 const xlsx = require("node-xlsx");
 const R = require("ramda");
 
-const API_URL = "localhost:8889";
+const API_URL = "http://localhost:8889";
 const VKM_URL = API_URL + "/muunnin";
 const GEOCODE_URL = API_URL + "/geocode";
 const REVERSE_GEOCODE_URL = API_URL + "/reversegeocode";
